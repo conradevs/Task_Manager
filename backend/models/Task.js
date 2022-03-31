114f239f14dfe1ec8efd1623f8aec6913e5fe890
conradevs <csilveiradev@gmail.com>
2022-03-30 23:58:24 -0300

    tasks model

---
+++ backend/models/Task.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose'
+
+const taskSchema = mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    description: {
+        type: String,
+        trim: true,
+        required: true
+    },
+
+    state: {
+        type: Boolean,
+        default: false,
+    },
+    deadLine: {
+        type: Date,
+        required: true,
+        default: Date.now()
+    },
+    priority: {
+        type: String,
+        required: true,
+        enum: ['Low','Midium','High']
+    },
+    project: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Project",
+    },
+},{
+    timestamps: true
+});
+
+const Task = mongoose.model("Task", taskSchema);
+export default Task;
