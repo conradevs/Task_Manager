bddd3f209498884d825ab82650939a49148debb1
conradevs <csilveiradev@gmail.com>
2022-04-03 23:57:50 -0300

    almost finished with tasks controller

--- backend/controllers/proyController.js
+++ backend/controllers/proyController.js
@@ -1,4 +1,5 @@
 import Project from "../models/Project.js";
+import Task from "../models/Task.js";
 
 
 const getProjects = async (req, res) => {
@@ -31,7 +32,12 @@ const getProject = async (req, res) => {
         const error = new Error("Access denied")
         return res.status(401).json({msg: error.message});
     }
-    res.json(project)
+    // Get project tasks
+    // Must be project creator or collaborator
+    const tasks = await Task.find().where("project").equals(project._id);
+    const response = {...project,...tasks};
+    res.json(response);
+    //res.json(project)
 };
 
 const editProject = async (req, res) => {
@@ -86,7 +92,19 @@ const addCollaborator  = async (req, res) => {};
 
 const deleteCollaborator  = async (req, res) => {};
 
-const getTasks  = async (req, res) => {};
+const getTasks  = async (req, res) => {
+    const {id} = req.params;
+
+    const project = await Project.findById(id);
+
+    if(!project) {
+        const error = new Error("Not Found")
+        return res.status(404).json({msg: error.message})
+    }
+    // Must be project creator or collaborator
+    const tasks = await Task.find().where("project").equals(id);
+    res.json(tasks);
+};
 
 export {
     getProjects,
