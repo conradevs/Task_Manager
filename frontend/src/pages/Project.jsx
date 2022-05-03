82a42ce5535d86ead7d2387b6e943c2ea4bd381a
conradevs <csilveiradev@gmail.com>
2022-05-03 20:54:07 -0300

    New task button

--- frontend/src/pages/Project.jsx
+++ frontend/src/pages/Project.jsx
@@ -14,21 +14,32 @@ const Project = () => {
   const {name} = project
   if (loading) return 'loading...'
   return (  
-    <div className='flex justify-between'>
-      
-      <h1 className='font-black text-4xl'>{name}</h1>
-      
-      <div className='flex items-center gap-2 text-gray-400 hover:text-gray-800'>
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-          <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-          <path fillRule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clipRule="evenodd" />
-        </svg>
-        <Link
-          to={`/projects/edit/${params.id}`}
-          className='uppercase font-bold'
-        >Edit</Link>
+    <>
+      <div className='flex justify-between'>
+
+        <h1 className='font-black text-4xl'>{name}</h1>
+
+        <div className='flex items-center gap-2 text-gray-400 hover:text-gray-800'>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+            <path fillRule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clipRule="evenodd" />
+          </svg>
+          <Link
+            to={`/projects/edit/${params.id}`}
+            className='uppercase font-bold'
+          >Edit</Link>
+        </div>
+
       </div>
-    </div>
+      <button
+        type='button'
+        className='text-sm px-5 py-3 w-full md:w-auto rounded-lg uppercase font-bold bg-sky-400 text-white text-center mt-5 flex gap-2 items-center justify-center' 
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+        New task</button>
+    </>
   )
 }
 
