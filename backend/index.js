28ed695fcca7f791bd809dc8d54a924bfdcbb362
conradevs <csilveiradev@gmail.com>
2022-12-08 01:22:44 -0300

    searching for cors problem root

--- backend/index.js
+++ backend/index.js
@@ -21,13 +21,15 @@ const corsOptions = {
             callback(null, true);
         } else{
             // Not autorization
-            callback(new Error("CORS Error"));
+            console.log('request from '+ origin)
+            console.log('request from not allowed domain')
+            callback(new Error("Some CORS Error again"));
         }
     },
 };
 
 
-app.use(cors());//corsOptions));
+app.use(cors(corsOptions));
 // Routing
 
 app.use('/api/users', userRoutes);
--- frontend/src/config/axiosclient.jsx
+++ frontend/src/config/axiosclient.jsx
@@ -1,8 +1,7 @@
 import axios from 'axios'
-import dotenv from 'dotenv';
 
 const axiosclient = axios.create({
-    baseURL: `${process.env.VITE_BACKEND_URL}/api`
+    baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`
 })
 
 export default axiosclient;
