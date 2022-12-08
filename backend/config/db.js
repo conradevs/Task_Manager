9e5050716d8779f0835a1a43f250f5ce86014729
conradevs <csilveiradev@gmail.com>
2022-12-07 22:41:03 -0300

    apparently api repaired magically

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const conectarDB = async() => {
+const conectDB = async() => {
     try {
         const connection = await mongoose.connect(process.env.MONGO_URI, {
                 useNewUrlParser: true,
@@ -17,4 +17,4 @@ const conectarDB = async() => {
 
 }
 
-export default conectarDB;
+export default conectDB;
